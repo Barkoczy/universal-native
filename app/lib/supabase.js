8762ec3e065311d8730d763924c305e9e687dcd7
Barkoczy.Coding <104427074+Barkoczy@users.noreply.github.com>
2022-04-27 00:13:24 +0200

    Update supabase.js

--- app/lib/supabase.js
+++ app/lib/supabase.js
@@ -1,5 +1,5 @@
-import { createClient } from '@supabase/supabase-js';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import { createClient } from '@supabase/supabase-js';
 import {
   SUPABASE_URL,
   SUPABASE_ANON_KEY
