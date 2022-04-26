f6adac898fdcea55db21610866f3d22b92aaedee
Henrich Barkoczy <henrich.barkoczy@tutanota.com>
2022-04-27 00:11:52 +0200

    Lib|Supabase ver.0.0.1

--- App.js
+++ App.js
@@ -1,7 +1,25 @@
+import React, { useState, useEffect } from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { supabase } from './app/lib/supabase';
+
 import WelcomeScreen from './app/screens/WelcomeScreen';
+import SingupScreen from './app/screens/SingupScreen';
 
 export default function App() {
+  const [auth, setAuth] = useState({});
+
+  useEffect(() => {
+    setAuth(supabase.auth.session());
+
+    supabase.auth.onAuthStateChange((_event, session) => {
+      console.log(session)
+      setAuth(session);
+    })
+  });
+
   return (
-    <WelcomeScreen />
+    <NavigationContainer>
+      {auth ? <WelcomeScreen /> : <SingupScreen />}
+    </NavigationContainer>
   );
 }
